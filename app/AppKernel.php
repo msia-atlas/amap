86e7761316830ab83b3ed6d2424aeb2b7ba032dc
thais <thais.martet@gmail.com>
2016-03-31 10:07:28 +0200

    Correction User

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,6 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new AmapBundle\AmapBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
