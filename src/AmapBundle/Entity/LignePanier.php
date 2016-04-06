06da8c0daa20a25fdd2099a604ca4a60d20f03a1
AdrienTODA <adrientoda@gmail.com>
2016-04-06 11:53:51 +0200

    Création entity LignePanier

---
+++ src/AmapBundle/Entity/LignePanier.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace AmapBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * LignePanier
+ */
+class LignePanier
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $quantite;
+
+    /**
+     * @var string
+     */
+    private $produit;
+
+    /**
+     * @var integer
+     */
+    private $quantiteParDefaut;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set quantite
+     *
+     * @param integer $quantite
+     * @return LignePanier
+     */
+    public function setQuantite($quantite)
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    /**
+     * Get quantite
+     *
+     * @return integer 
+     */
+    public function getQuantite()
+    {
+        return $this->quantite;
+    }
+
+    /**
+     * Set produit
+     *
+     * @param string $produit
+     * @return LignePanier
+     */
+    public function setProduit($produit)
+    {
+        $this->produit = $produit;
+
+        return $this;
+    }
+
+    /**
+     * Get produit
+     *
+     * @return string 
+     */
+    public function getProduit()
+    {
+        return $this->produit;
+    }
+
+    /**
+     * Set quantiteParDefaut
+     *
+     * @param integer $quantiteParDefaut
+     * @return LignePanier
+     */
+    public function setQuantiteParDefaut($quantiteParDefaut)
+    {
+        $this->quantiteParDefaut = $quantiteParDefaut;
+
+        return $this;
+    }
+
+    /**
+     * Get quantiteParDefaut
+     *
+     * @return integer 
+     */
+    public function getQuantiteParDefaut()
+    {
+        return $this->quantiteParDefaut;
+    }
+}
