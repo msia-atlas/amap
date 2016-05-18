c93f4129b75042c28154a739a9d2b70892e00cd8
thais <thais.martet@gmail.com>
2016-05-18 13:40:33 +0200

    Fin gestion des droits

--- src/AmapBundle/Entity/ContratConsommateur.php
+++ src/AmapBundle/Entity/ContratConsommateur.php
@@ -7,11 +7,9 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * ContratConsommateur
  */
-<<<<<<< HEAD
-class ContratConsommateur 
-=======
+
 class ContratConsommateur extends Contrat
->>>>>>> 3c0155268c47f5d7c634c6eb1b7a646c6ac757eb
+
 {
 
     /**
---
+++ src/AmapBundle/Repository/FosGroupRepository.php
@@ -0,0 +1,10 @@
+<?php
+namespace  AmapBundle\Repository;
+ 
+use Doctrine\ORM\EntityRepository;
+ 
+class FosGroupRepository extends EntityRepository
+{
+
+
+}
